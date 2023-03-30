95276ea58e76a06599fa00a2bd38e5932ee4e6e5
Digidix28 <idriss.jeaidi@gmail.com>
2023-03-30 16:15:26 +0000

    mise en place du squellette du projet

--- Projet/backend/config.php
+++ Projet/backend/config.php
@@ -1,7 +1,7 @@
 <?php
 define('_MYSQL_HOST','127.0.0.1');
 define('_MYSQL_PORT',3306);
-define('_MYSQL_DBNAME','dbtest');
+define('_MYSQL_DBNAME','projet');
 define('_MYSQL_USER','root');
 define('_MYSQL_PASSWORD','');
 define('_MYSQL_SERVNAME','localhost');
@@ -13,8 +13,6 @@ if (defined('_MYSQL_PORT'))
 
 $connectionString .= ";dbname=" . _MYSQL_DBNAME;
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-print("test branch 2");
-
 
 $_var ; 
 ?>
