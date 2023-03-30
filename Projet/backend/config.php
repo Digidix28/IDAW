3fa8244e03082f3064a456004b3f6dce87a5dc20
Digidix28 <idriss.jeaidi@gmail.com>
2023-03-30 14:46:55 +0000

    Début du projet

---
+++ Projet/backend/config.php
@@ -0,0 +1,17 @@
+<?php
+define('_MYSQL_HOST','127.0.0.1');
+define('_MYSQL_PORT',3306);
+define('_MYSQL_DBNAME','dbtest');
+define('_MYSQL_USER','root');
+define('_MYSQL_PASSWORD','');
+define('_MYSQL_SERVNAME','localhost');
+
+$connectionString = "mysql:host=" . _MYSQL_HOST;
+
+if (defined('_MYSQL_PORT'))
+    $connectionString .= ";port=" . _MYSQL_PORT;
+
+$connectionString .= ";dbname=" . _MYSQL_DBNAME;
+$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+?>
