8c52eba2671e0070d31d0f02f31c4e11219d463c
youyoulebgdemeknes <winghouserv511@gmail.com>
2023-03-30 17:11:39 +0200

    test2_younes

--- Projet/backend/config.php
+++ Projet/backend/config.php
@@ -14,4 +14,5 @@ if (defined('_MYSQL_PORT'))
 $connectionString .= ";dbname=" . _MYSQL_DBNAME;
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 echo "<button>Click</button>";
+echo"salam";
 ?>
