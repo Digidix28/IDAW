9b0513252e3dbf0edb169ca5d04e14bd045d162a
Digidix28 <idriss.jeaidi@gmail.com>
2023-03-30 15:43:15 +0000

    final test idriss

--- TP5/exo2/backend/config.php
+++ TP5/exo2/backend/config.php
@@ -13,6 +13,6 @@ if (defined('_MYSQL_PORT'))
 
 $connectionString .= ";dbname=" . _MYSQL_DBNAME;
 $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-print("on est en main")
+print("on est en main main main ")
 
 ?>
