a509d43e3559a2769558dc9ce26d99d5f0eb1087
youyoulebgdemeknes <winghouserv511@gmail.com>
2023-04-07 16:50:34 +0200

    users / profil update

--- Projet/frontend/profil.php
+++ Projet/frontend/profil.php
@@ -1,31 +1,71 @@
 <?php
 require_once("templates/template_header.php");
-session_start();
-if (isset($_SESSION['id']) == false) {
-    header("Location: login.php");
-    exit;
-}else{
-    $id = $_SESSION['id'];
-}
 ?>
 
-  <body>
-    <main> 
+<body>
+    <main>
         <header>
             <h1>Page d'accueil</h1>
-        </header>
-        <p> Bienvenue sur mon premier site web. Cette page a pour seule utilité de s'orienté vers les autres parties du site</p>
-
-        <h2>Sommaire des pages</h2>
-        <div class="container colonne">
-        <?php
+            <h2>Sommaire des pages</h2>
+            <?php
             require_once("templates/template_menu.php");
             renderMenuToHTML('infos_techniques');
-        ?>
+            ?>
+        </header>
+        <p> bienvenue sur votre page de profil </p>
+
+        <form id="profil-form" action="" onsubmit="onSignUpFormSubmit();">
+            <h2>My Profil</h2>
+            <div class="form-group">
+                <label for="email">Email :</label>
+                <input type="email" id="login" name="login" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe :</label>
+                <input type="password" id="mdp" name="mdp" required>
+            </div>
+            <div class="form-group">
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="nom" required>
+            </div>
+            <div class="form-group">
+                <label for="prenom">Prénom :</label>
+                <input type="text" id="prenom" name="prenom" required>
+            </div>
+            <div class="form-group">
+                <label for="sexe">Sexe :</label>
+                <input type="number" id="sexe" name="sexe" required>
+            </div>
+            <div class="form-group">
+                <label for="age">Age :</label>
+                <input type="number" id="age" name="age" required>
+            </div>
+            <div class="form-group">
+                <label for="poid">Poids :</label>
+                <input type="number" id="poid" name="poid" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="modifier">
+            </div>
+        </form>
         </div>
     </main>
-        <?php
-            require_once("templates/footer_template.php")
-        ?>
-  </body>
+    <script>
+        $.ajax({
+            url: "http://localhost/projet/IDAW/Projet/backend/API/users.php",
+            type: "GET",
+            data: {
+                user_id: "2", // Replace with the actual user ID you want to retrieve
+                fields: "login,nom,prenom,sexe,age,mdp" // Specify the fields you want to retrieve, separated by commas
+            },
+            success: function (response) {
+                console.log(response.data.user_data); // The retrieved user data will be available in the user_data object
+            },
+            error: function (xhr, status, error) {
+                console.log(error); // Log any errors to the console
+            }
+        });
+    </script>
+</body>
+
 </html>
